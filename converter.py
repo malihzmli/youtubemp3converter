ac8c0ed82d777189d54cd89c326c8115939e7329
malihzmli <extumonte@gmail.com>
2023-08-09 17:36:27 +0300

    new

--- converter.py
+++ converter.py
@@ -2,32 +2,41 @@ from pytube import YouTube
 import os
 from moviepy.editor import *
 import time
-def videoindirme(link):
-    try:
+failed=0
+def download(link):
+    try: 
         yt= YouTube(link)
+        print(link+" "+"is downloading")
         download = yt.streams.get_lowest_resolution()
+        title = yt.title
         path=download.get_file_path()
         download.download()
-        print(path+" "+'indirildi')
-        
-        
-        try:
-            FILETOCONVERT = AudioFileClip(path)
-            FILETOCONVERT.write_audiofile(path+'.mp3')
-            FILETOCONVERT.close()
-            time.sleep(1)
-            print(title+" "+'donusturuldu')
-            
-        except:
-            os.remove(path)
-        except:
-            
-            print(path+" "+' donusurken bir hata ile karsilasildi')
+        print(title+" "+'downloaded')
+        convert(path)
     except:
-        print(link+" "+'indirilirken  hata ile karsilasildi')
-file =open('songs.txt','r')
-for i in file.readlines():
-    videoindirme(i)
+        global failed
+        failed+=1
+        faileds.writelines(link)
+        print(link+" "+'An error  occurred   while downloading')
+def convert(path):
+    try:
+        FILETOCONVERT = AudioFileClip(path)
+        FILETOCONVERT.write_audiofile(path+'.mp3')
+        FILETOCONVERT.close()
+        time.sleep(1)
+        os.remove(path)
+    except:
+        global failed
+        failed+=1
+        faileds.writelines(link)
+        print('An error occurred  while  converting') 
+faileds=open('failed.txt','w')
+songs =open('songs.txt','r')
+for i in songs.readlines():
+    print("************************************************************")
+    download(i)
+    print("************************************************************")
+print(str(failed)+" "+"download failed go `faileds.txt`")
 
                 
 
